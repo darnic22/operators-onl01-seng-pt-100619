c5b0876674cc92fbdcad046a5a48e2964a5c9b72
Nicole M Darby <nicoledarby021322@gmail.com>
2019-10-18 00:48:51 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,18 @@
-
-ef unsafe?(speed)
+require "pry"
+def unsafe?(speed)
+  binding.pry
  if unsafe(40>0)
    puts "5"
- else unsafe(0<60)
+ end
    
 end
- binding.pry 
-end
+
 
 
 
 
 def not_safe?(speed)
-  if not_safe<40? "unsafe": "safe"
+   not_safe<40? "unsafe": "safe"
 end 
 	
 
